3430d6c8863e288ae0fcffd755552b923c68c43a
fadoilul <fadoilulmun.im2807@gmail.com>
2020-03-10 18:39:30 +0700

    versi 1

---
+++ Swap-Variable/swap.py
@@ -0,0 +1,16 @@
+def swap (a,b):
+    c = b
+    b = a
+    a = c
+    return(a,b)
+
+awal = input("masukkan nilai awal : ")
+akhir = input("masukkan nilai akhir : ")
+
+print("Anda memasukkan nilai awal :",awal)
+print("\t\tnilai akhir :",akhir)
+
+awal,akhir = swap(b=akhir,a=awal)
+
+print("Setelah swap : ")
+print("\tNilai awal : ",awal,"\n\tNilai akhir : ",akhir)
