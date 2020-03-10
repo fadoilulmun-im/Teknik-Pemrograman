b340bf7397c06218441725bf74f2085e93d71b97
fadoiilul <ilulmun.im@gmail.com>
2020-03-10 18:55:08 +0700

    create swap function

--- Swap-Variable/swap.py
+++ Swap-Variable/swap.py
@@ -10,7 +10,7 @@ akhir = input("masukkan nilai akhir : ")
 print("Anda memasukkan nilai awal :",awal)
 print("\t\tnilai akhir :",akhir)
 
-awal,akhir = swap(b=akhir,a=awal)
+awal,akhir = swap (b = akhir,a = awal)
 
 print("Setelah swap : ")
 print("\tNilai awal : ",awal,"\n\tNilai akhir : ",akhir)
